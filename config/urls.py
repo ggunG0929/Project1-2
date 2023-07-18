7c43ec2bc52447e7ecfc0dd52df85487d24f52ae
ggunG <ggung0929@gmail.com>
2023-07-18 18:36:42 +0900

    7.18 settings, urls 정리

--- config/urls.py
+++ config/urls.py
@@ -5,18 +5,20 @@ from django.urls import path, include
 from greenbooks import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),    # 관리자 페이지
+    path('', views.index, name='index'),    # 인덱스
+
+    path('price/', include('checkbookprice.urls')),     # 바코드 가격조회
+    path('community/', include('community.urls')),      # 커뮤니티
+    path('custom/', include('custom.urls')),            # 고객센터
+    path("goods/", include("goods.urls")),              # 구매하기
     path("gbook/mypage", views.mypage, name='mypage'),
-    path('common/', include('common.urls')),
-    path('community/', include('community.urls')),  # 커뮤니티
-    path('tradebook/', include('tradebook.urls')),
     path('mypage/', include('mypage.urls')),
-    path("goods/", include("goods.urls")),
-    path('custom/', include('custom.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),   # 편집기
-    path('price/', include('checkbookprice.urls')), # 바코드가격조회
-    path('shopcart/', include('shopcart.urls'))
+    path('shopcart/', include('shopcart.urls')),        # 장바구니
+    path('tradebook/', include('tradebook.urls')),      # 직거래
+    path('common/', include('common.urls')),
+
+    path('ckeditor/', include('ckeditor_uploader.urls')),  # 편집기
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
